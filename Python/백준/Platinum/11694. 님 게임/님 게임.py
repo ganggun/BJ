928cb685c11c056ebe87a54b223f4233ceca9752
ganggun <165283835+ganggun@users.noreply.github.com>
2024-07-01 21:10:34 +0900

    [Platinum II] Title: 님 게임, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ Python/백준/Platinum/11694. 님 게임/님 게임.py
@@ -0,0 +1,21 @@
+a=int(input())
+lst=list(map(int,input().split()))
+if len(list(filter(lambda x:x==1,lst)))==len(lst):
+    if len(lst)%2==0:
+        print("koosaga")
+    else:
+        print("cubelover")
+else:
+    if a==1:
+        print("koosaga") 
+    elif a==2:
+        print("cubelover") if lst[0]==lst[-1] else print("koosaga")
+    else:
+        c = lst.pop(0)
+        for i in lst:
+            c^=i
+        if c==0:
+            print("cubelover")
+    
+        else:
+            print("koosaga")
